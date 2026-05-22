382c897a181278714ba69b0caf37e631b13029bd
kennychou1998-stack <kennychou1998@gmail.com>
2026-05-22 10:50:10 +0800

    33

---
+++ home/data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "澀谷前站路口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.9160634234544229,
+        "pitch": -0.3536522800919606,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.8621977958399754,
+          "pitch": -0.08798343197355507,
+          "title": "澀谷前站",
+          "text": "澀谷事變"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
